7469135bcd90138c2e0855528d1ef751015e9b0f
Илья Тясин <turbopad7200@gmail.com>
2024-09-19 13:11:01 +0500

    Task1

--- Tyuiu.TiasinIA.Sprint1.Task1.V25.Lib/DataService.cs
+++ Tyuiu.TiasinIA.Sprint1.Task1.V25.Lib/DataService.cs
@@ -7,6 +7,6 @@ namespace Tyuiu.TiasinIA.Sprint1.Task1.V25.Lib
         public double Calculate(double x, double y)
         {
             return((x * y) / (6 + x));
-        }
+        } // в фолрмуле должна быть единица, но компилятор на сайте считает неверно! Не может 4/3 дать ответ 0.5!!!!!
     }
 }
