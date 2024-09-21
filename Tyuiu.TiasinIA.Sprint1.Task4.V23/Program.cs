1e4abb488ffb443559d35f13ba730eaca7213548
Илья Тясин <turbopad7200@gmail.com>
2024-09-21 16:25:30 +0500

    Task4

--- Tyuiu.TiasinIA.Sprint1.Task4.V23/Program.cs
+++ Tyuiu.TiasinIA.Sprint1.Task4.V23/Program.cs
@@ -11,18 +11,19 @@ namespace Tyuiu.TiasinIA.Sprint1.Task4.V23
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* спринт #1                                                               *");
-            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
-            Console.WriteLine("* Задание #3                                                              *");
-            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Тема : Class Math                                                       *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
             Console.WriteLine("* Выполнил: Тясин Илья Александрович | АСОиУб-24-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
+            
             double x;
 
             Console.WriteLine("Введите значение x:");
